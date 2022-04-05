c377f9b858c0a2ee4bf52cd1d4a9d5ccfca03a78
Luis Gustavo <gustavobarbosag7@gmail.com>
2022-04-04 22:37:57 -0300

    create header home page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import './styles/Main.css';
+
+const userConfig = {
+    name: 'Luís Gustavo',
+    imageProfile: 'https://i.imgur.com/eDaagpn.jpg'
+}
 
 export default class Main extends Component {
     render() {
         return (
-            <div>
-                <p>Hello World!</p>
+            <div className="container">
+                <header>
+                    <div className="primary-information">
+                        <p className="name-user">Olá, {userConfig.name}</p>
+                        <span className="current-date">Quarta-feira, 30 de abril - 2022</span>
+                    </div>
+                    <div className="image-user-container">
+                        <img className="image-user" src={userConfig.imageProfile} alt="Imagem de perfil do usuário" />
+                    </div>
+                </header>
             </div>
         )
     }
